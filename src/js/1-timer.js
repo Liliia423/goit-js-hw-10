8bc5063b9fe85902c8f967265ebbf7b29acd4ef4
Liliia423 <etude4408@gmail.com>
2024-12-24 13:50:31 +0200

    rty

---
+++ src/js/1-timer.js
@@ -0,0 +1,7 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+/*const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});*/
