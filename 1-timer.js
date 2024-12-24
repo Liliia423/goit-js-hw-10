0bd11f9ccae7127e78110c7065b6edf84407d59c
Liliia423 <etude4408@gmail.com>
2024-12-24 17:45:58 +0000

    Deploying to gh-pages from @ Liliia423/goit-js-hw-10@24c508f4323f32d61b256d797f4f0a835729ffe4 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-4re2sZo0.js";const e=document.querySelector("button");e.classList.add("startBtn");const o=document.querySelectorAll(".label");o.forEach(t=>{t.textContent=t.textContent.toUpperCase()});
+//# sourceMappingURL=1-timer.js.map
