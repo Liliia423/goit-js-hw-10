42e54a768de731b96f89e440f377ec41cc711cfc
Liliia423 <etude4408@gmail.com>
2024-12-27 20:00:11 +0200

    rty

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,16 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-// Ініціалізація flatpickr із параметрами
-flatpickr('#datetime-picker', options);
+const inputDelay = document.querySelector('input[name="delay"]');
+inputDelay.classList.add('inputDelayField');
+
+/*document.addEventListener('DOMContentLoaded', () => {
+  const inputDelay = document.querySelector('input[name="delay"]');
+  inputDelay.classList.add('inputDelayField');
+});*/
+
+const stateSelectionField = document.querySelector('fieldset');
+stateSelectionField.classList.add('.stateField');
+
+const createButton = document.querySelector('.form button[type="submit"]');
+createButton.classList.add('createBtn');
