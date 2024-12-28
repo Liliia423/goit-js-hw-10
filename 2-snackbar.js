93007906ad1c311831cdbcc98c6e55d900e3f049
Liliia423 <etude4408@gmail.com>
2024-12-28 17:23:55 +0000

    Deploying to gh-pages from @ Liliia423/goit-js-hw-10@7e5374b71f43968bc703c26af39b56d2a141e533 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,8 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector('input[name="delay"]');t.classList.add("inputDelayField");const e=document.querySelector("fieldset");e.classList.add(".stateField");const o=document.querySelector('.form button[type="submit"]');o.classList.add("createBtn");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as c}from"./assets/vendor-BbbuE1sJ.js";const o=document.querySelector('input[name="delay"]');o.classList.add("inputDelayField");const u=document.querySelector("fieldset");u.classList.add(".stateField");const a=document.querySelector('.form button[type="submit"]');a.classList.add("notificationButton");const i=document.querySelector('input[name="state"]:checked');i&&i.value;a.addEventListener("click",l=>{l.preventDefault();const s=Number(o.value),t=document.querySelector('input[name="state"]:checked'),n=t&&t.value==="fulfilled";new Promise((e,r)=>{setTimeout(()=>{n?e(`Fulfilled promise in ${s}ms`):r(`Rejected promise in ${s}ms`)},s)}).then(e=>{c.success({message:`<div class="success-toast"> 
+                  <span class="title">Ok</span>
+                  <span class="message">${e}</span>
+                  </div>`,position:"topRight",class:"success-toast",closeOnClick:!0})}).catch(e=>{c.error({message:`<div class="error-toast">
+                  <span class="title">Error</span>
+                  <span class="message">${e}</span>
+                  </div>`,position:"topRight",class:"error-toast",closeOnClick:!0})})});
 //# sourceMappingURL=2-snackbar.js.map
